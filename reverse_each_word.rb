5b6a078f4f9dfcf2ee6703374fb0e1d502af8b33
Ryan Wolfe <ryan@studiohopfitness.com>
2019-01-28 20:37:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
-ary = string.array(" ")
-ary.collect do |x|
-x.reverse
-ary2 = x
-end
-ary2.join
+  ary = string.Array(" ")
+  ary.collect do |x|
+    x.reverse
+    ary2 = x
+  end
+  ary2.join
 end
